14be554a66c9783c8ce92a70e96b87912c8759b3
alex007sirois <alex007sirois@gmail.com>
2017-02-17 14:43:42 -0500

    ajout d'un rectangle de couleur à partir du js externe
    +
    
    gestion de deux inputs et affichage dans une div

--- Jeu.js
+++ Jeu.js
@@ -1,4 +1,3 @@
-(function(){
 function Jeu()
 {
 	var jeu = this;
@@ -8,16 +7,41 @@ function Jeu()
 	var joueur1 = null;
 	var joueur2 = null;
 	var collisions = null;
+	
+	var canvas2D;
+	
+	var debug = document.getElementById("debug");
+	
+	var canvas = document.getElementById('canvas');
+	canvas2D = canvas.getContext('2d');
+	canvas2D.fillStyle='red';
+	canvas2D.fillRect(0,0,canvas.width,canvas.height);
+	
+	
+	
+	var CliquerSouris = function(evenement)
+	{
+		debug.innerHTML = "click<br>" + debug.innerHTML;
+	}
+	
+	var BougerSouris = function(evenement)
+	{
+		var mousePos = evenement.clientX + ',' + evenement.clientY;
+		debug.innerHTML = mousePos + '<br>' + debug.innerHTML;
+	}
+	
+	this.lancer = function()
+	{
+		window.addEventListener("click", CliquerSouris);
+		window.addEventListener('mousemove', BougerSouris);
+	}
 }
+
+
 
 Jeu.Configuration = 
 {
-  interval : 1000/60,
-  FPS : 60,
-  /*toucheGauche : 37, 
-  toucheDroite : 39,
-  toucheHaut : 38,
-  toucheBas : 40*/
+  interval : 1000/60
 }
 
 Jeu.Evenement =
@@ -26,18 +50,6 @@ Jeu.Evenement =
   arrierePlanFinChargement : document.createEvent('Event'),
 }
 
-Jeu.Evenement.initialiser = function()
-{
-  for(key in Jeu.Evenement)
-  {
-    if(Jeu.Evenement[key] instanceof Event)
-    {
-      Jeu.Evenement[key].initEvent(key, true, true);
-    }
-  }
-  window['Evenement'] = Jeu.Evenement;
-}();
 
 Jeu = new Jeu();
 Jeu.lancer();
-})();
