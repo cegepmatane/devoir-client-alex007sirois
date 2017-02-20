98872cfb93696a1baf1fab293e3baacbe4bb9377
alex007sirois <alex007sirois@gmail.com>
2017-02-19 20:51:30 -0500

    animation de l'arriere plan

---
+++ ArrierePlan.js
@@ -0,0 +1,71 @@
+//Rien est public...
+var ArrierePlan = function(scene)
+{
+    var arrierePlan = this; //Est un proxy de closure...
+
+    //variables priv�es seules les m�thodes initialis�es ici y ont acc�s...
+    var nombreImagesChargees = 0;
+    
+    var arrierePlanConteneur = new createjs.Container();
+    
+    var matriceMontagne = new createjs.Matrix2D();
+    var paysageMontagne = new createjs.Shape();
+    var imageForetMontagne = new Image();
+
+    //Cet �v�nement ne peut pas �tre d�clench� hors de cette fonction
+    var chargementCompletArrierePlan = document.createEvent('Event');
+    chargementCompletArrierePlan.initEvent('chargementCompletArrierePlan', true, true);
+
+    
+    //Constructeur parce qu'il est appel� inline � la fin...
+    var initialiser = function()
+    {
+      //http://www.crockford.com/javascript/private.html
+            
+      imageForetMontagne.onload = function()
+      {
+        paysageMontagne.graphics.beginBitmapFill(imageForetMontagne,"repeat", matriceMontagne).drawRect(0,0,1400,800).endStroke();
+        nombreImagesChargees++;
+      }
+      imageForetMontagne.src = "paysage-arriere.png";
+      
+      
+
+      var demarrerAnimation = function (evenement)
+      {
+        arrierePlanConteneur.addChild(paysageMontagne)
+        
+        scene.addChild(arrierePlanConteneur);
+      }
+      
+      var validerChargementImages = function(evenement)
+      { 
+        if(nombreImagesChargees == Object.keys(ArrierePlan.Configuration.images).length)
+        {
+          window.dispatchEvent(chargementCompletArrierePlan); 
+          createjs.Ticker.removeEventListener("tick", validerChargementImages);
+          window.dispatchEvent(window.Evenement.arrierePlanFinChargement);
+        }
+      }
+      
+      window.addEventListener('chargementCompletArrierePlan', demarrerAnimation, false);
+      createjs.Ticker.addEventListener("tick", validerChargementImages);
+    }
+    
+    //constructeur
+    initialiser();
+    
+    //ICI c'est public
+    this.rafraichirAnimation =  function(evenement)
+    {
+        matriceMontagne.translate(-ArrierePlan.Configuration.vitesseMontagne,0);
+	}
+
+ArrierePlan.Configuration = 
+{
+  images : {
+    imageForetMontagne : "paysage-arriere.png"
+  },
+  vitesseMontagne : 2
+}
+}
--- Jeu.js
+++ Jeu.js
@@ -14,8 +14,6 @@ function Jeu()
 	
 	var canvas = document.getElementById('canvas');
 	canvas2D = canvas.getContext('2d');
-	canvas2D.fillStyle='red';
-	canvas2D.fillRect(0,0,canvas.width,canvas.height);
 	
 	
 	
@@ -29,11 +27,38 @@ function Jeu()
 		var mousePos = 'X: ' + evenement.clientX + ' Y: ' + evenement.clientY;
 		debug.innerHTML = mousePos + '<br>' + debug.innerHTML;
 	}
+
+	var Dessiner = function(evenement)
+	{
+		var cercle = new createjs.Shape();
+		cercle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 50);
+		cercle.x = 100;
+		cercle.y = 100;
+		scene.addChild(cercle);
+
+		createjs.Ticker.addEventListener("tick", rafraichirAnimation);
+	}
+	
+	var rafraichirAnimation = function(evenement)
+	  {
+			arrierePlan.rafraichirAnimation(evenement);
+		
+			scene.update(evenement);
+	  }
 	
 	this.lancer = function()
 	{
+		
 		window.addEventListener("click", CliquerSouris);
 		window.addEventListener('mousemove', BougerSouris);
+
+		window.addEventListener(window.Evenement.arrierePlanFinChargement.type,Dessiner);
+		
+		scene = new createjs.Stage(canvas);
+		
+		arrierePlan = new ArrierePlan(scene);
+
+		createjs.Ticker.setInterval(1000/60);
 	}
 }
 
@@ -41,7 +66,7 @@ function Jeu()
 
 Jeu.Configuration = 
 {
-  interval : 1000/60
+  interval : 1000/60,
 }
 
 Jeu.Evenement =
@@ -50,6 +75,17 @@ Jeu.Evenement =
   arrierePlanFinChargement : document.createEvent('Event'),
 }
 
+Jeu.Evenement.initialiser = function()
+{
+  for(key in Jeu.Evenement)
+  {
+    if(Jeu.Evenement[key] instanceof Event)
+    {
+      Jeu.Evenement[key].initEvent(key, true, true);
+    }
+  }
+  window['Evenement'] = Jeu.Evenement;
+}();
 
 Jeu = new Jeu();
 Jeu.lancer();
