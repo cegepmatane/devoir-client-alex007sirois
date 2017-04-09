da0b7e9295b77fe077ac3113e0e1326eef68c551
alex007sirois <alex007sirois@gmail.com>
2017-04-09 15:27:22 -0400

    animation svg

--- vue/JeuVue.js
+++ vue/JeuVue.js
@@ -3,7 +3,6 @@ JeuVue = function()
 	this.afficher = function()
   	{
 		document.getElementsByTagName("body")[0].innerHTML = JeuVue.html;
-		document.getElementById("animationFuseeSVG").innerHTML = JeuVue.svg;
   	}
 
   	this.modifierVitesse = function(vitesse)
@@ -13,4 +12,3 @@ JeuVue = function()
 }
 
 JeuVue.html = document.getElementById("page-jeu").innerHTML;
-JeuVue.svg = document.getElementById("animationSVG").innerHTML;
